6659aea71538a83b98abb6b8dde3a37a854d1ae3
swapnilbhojane4 <swapnilbhojane4@gmail.com>
2024-10-16 14:59:11 +0530

    homepage-added latest products

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
