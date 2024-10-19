2c2c99788f6a90ba8747e4ff60b86727eaf57d9d
swapnilbhojane4 <swapnilbhojane4@gmail.com>
2024-10-19 23:31:49 +0530

    added collection page

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Title = ({text1, text2}) => {
+  
   return (
     <div className='inline-flex gap-2 items-center mb-3'>
         <p className='text-gray-500'>{text1}
