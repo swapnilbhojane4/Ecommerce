a23e78e04473fbbc35bfe66ae02b6e6950b53753
swapnilbhojane4 <swapnilbhojane4@gmail.com>
2024-11-22 23:49:45 +0530

    added product description

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -9,6 +9,7 @@ const Product = () => {
   const {products, currency} = useContext(ShopContext);
   const [productData, setProductData] = useState();
   const [image, setImage] = useState('');
+  const [size, setSize] = useState('')
 
   const fetchProductsData = async () => {
 
@@ -59,6 +60,23 @@ const Product = () => {
           </div>
           <p className='mt-5 text-3xl font-medium'>{currency}{productData.price}</p>
           <p className='mt-5 text-gray-500 md:w-4/5'>{productData.description}</p>
+          <div className='flex flex-col gap-4 my-7'>
+            <p>Select size</p>
+            <div className="flex gap-2">
+              {productData.sizes.map((item, index)=>(
+                <button onClick={()=>setSize(item)}
+                className={`border py-2 px-4 bg-gray-200 ${item === size ? 'border-orange-500' : ''} `} 
+                key={index}>{item}</button>
+              ))}
+            </div>
+          </div>
+          <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+          {/* <hr className='mt-8 sm:w-4/5' /> */}
+          <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+            <p>100% Original product.</p>
+            <p>Cash on delivery is available on this product.</p>
+            <p> Easy return and exchange policy within 7 days.</p>
+          </div>
         </div>
       </div>
     </div>
